05de9a90af2190242f062383cc64d5f0d543bc51
brunoid <wonpis11@gmail.com>
2016-06-03 16:24:48 +0900

    Add files via upload

---
+++ nodelogin/credentials.js
@@ -0,0 +1,14 @@
+module.exports.dbConfig = {
+	connectionLimit : 10,
+  host            : 'host',
+  user            : 'user',
+  password        : 'password',
+  database        : 'my_db'
+}
+module.exports.sessionConfig = {
+  host            : 'host',
+  user            : 'user',
+  password        : 'password',
+  database        : 'my_db'
+}
+module.exports.sessionSecret = '#$@$@#$#@$@#$#@$@#';
